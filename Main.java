37a1d1ebaed3207ed13dc8827448fa8566247b7c
Parnika Jain <parnikajainjp@gmail.com>
2025-04-19 15:54:08 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main(String[] args){
+        StudentOperations sO = new StudentOperations();
+        sO.insertStudent();
+
+        sO.viewStudent();
+
+        sO.updateStudent();
+
+        sO.viewStudent();
+
+        sO.deleteStudent();
+
+        sO.viewStudent();
+
+    }
+}
